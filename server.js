7e84d356a4de487b78dc454bb33b22b95daaa13e
emily <limeyb7@gmail.com>
2017-02-05 20:55:10 +0000

    few formatting fixes

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const Hapi = require('hapi');
 const Inert = require('inert');
-const Path = require('path');
 
 const server = new Hapi.Server();
 
 server.register([Inert], (err) => {
 
-  server.connection({
-    port: process.env.PORT || 7000,
-  });
+  server.connection({ port: process.env.PORT || 7000, });
 
   server.route(require('./src/routes.js'));
 
-  server.start(() => {
-    console.log((`Server running at: ${server.info.uri}`));
-  })
+  server.start(() => { console.log((`Server running at: ${server.info.uri}`)); })
+
 });
 
 module.exports = server;
--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,5 @@ const home = require('./handlers/home.js');
 
 module.exports = [
    { method: 'GET', path: '/', handler: home },
-   { method: 'GET', path: '/{file*}', handler: { directory: { path: 'public/' } } }
+   { method: 'GET', path: '/{file*}', handler: { directory: { path: 'public/' } } },
 ];
